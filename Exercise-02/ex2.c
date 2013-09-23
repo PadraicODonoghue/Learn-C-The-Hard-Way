784478dae5af2b167913a6d6df071badb92d296a
Padraic O'Donoghue <padraicodonoghue@gmail.com>
2013-09-23 15:30:00 +0100

    Ex2

---
+++ Exercise-02/ex2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	puts("Hello world.");
+	puts("Printing nonsense");
+	puts("Printing nonsense");
+	puts("Printing nonsense");
+	puts("Printing nonsense");
+	
+	return 0;
+}
